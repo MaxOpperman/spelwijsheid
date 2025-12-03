72554aa6265126fe2697b26b6f29b5807adfd438
Max <m.opperman98@gmail.com>
2025-12-03 21:54:43 +0100

    feat: add option to ignore accents

--- src/routes/spelwijze-solver/+page.server.ts
+++ src/routes/spelwijze-solver/+page.server.ts
@@ -4,8 +4,12 @@ import { getSolverWords } from '$lib/words.server.js';
 export const load = (() => {
 	return {
 		/**
-		 * The list of Dutch words for the solver
+		 * The list of Dutch words for the solver (with normalized accents)
 		 */
-		wordList: getSolverWords()
+		wordList: getSolverWords(),
+		/**
+		 * The list of Dutch words for the solver (with original accents)
+		 */
+		wordListWithAccents: getSolverWords({ normalizeAccents: false })
 	};
 }) satisfies PageServerLoad;
